f717a73911253479da95fe636a18e3f447cde4f0
angelDariaux <angeldariaux@gmail.com>
2022-01-25 22:46:09 -0600

    :construction: casi terminamos, estilizando el modal que sale cuando damos click a los legos

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,9 +2,28 @@
 
 const modal = document.querySelector('.Modal');
 const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+
 const closeIcon = document.querySelector('.modal__content--close');
 
+        // Evento del boton 1
+
 button1.addEventListener('click', () => { 
+  modal.classList.remove('hidden');     /* con esto eliminamos la case de hidden */
+  modal.classList.add('visible');       /* aqui volvemos visible el modal */
+});
+
+        // Evento del boton 2
+
+button2.addEventListener('click', () => { 
+  modal.classList.remove('hidden');     /* con esto eliminamos la case de hidden */
+  modal.classList.add('visible');       /* aqui volvemos visible el modal */
+});
+
+        // Evento del bonto 3
+
+button3.addEventListener('click', () => { 
   modal.classList.remove('hidden');     /* con esto eliminamos la case de hidden */
   modal.classList.add('visible');       /* aqui volvemos visible el modal */
 });
