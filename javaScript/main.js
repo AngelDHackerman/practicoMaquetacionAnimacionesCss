1f3346e6750f4e767abb407be05bc35ece1873cf
angelDariaux <angeldariaux@gmail.com>
2022-01-25 21:37:44 -0600

    :construction: hicimos que un pop up apareciera y desapareciera Wiiiiiiiiiiiii !!!

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,8 +1,17 @@
 'use stirct';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.Modal');
 const button1 = document.querySelector('#button1');
+const closeIcon = document.querySelector('.modal__content--close');
 
-document.addEventListener('onclik', button1);
+button1.addEventListener('click', () => { 
+  modal.classList.remove('hidden');     /* con esto eliminamos la case de hidden */
+  modal.classList.add('visible');       /* aqui volvemos visible el modal */
+});
 
+closeIcon.addEventListener('click', () => {  // Asi escondemos de regreso el modal 
+  modal.classList.remove('visible');
+  modal.classList.add('hidden'); 
+  console.log('click detectado')
+})
 
